0e14e371c71da2f98e0ffc15448732ff331ce214
Filip Szymanski <arren13@gmail.com>
2025-03-25 00:50:36 +0100

    add fixtures for repeated functions - DRY

--- my_test.py
+++ my_test.py
@@ -8,48 +8,51 @@ def csv_data():
         data = f.read().split('\n')
     return data
 
+@pytest.fixture()
+def csv_header(csv_data):
+    return csv_data[0]
 
-def test_header_is_uppercase(csv_data):
+@pytest.fixture()
+def csv_records(csv_data):
+    return csv_data[1:]
+
+@pytest.fixture()
+def column_names(csv_header):
+    return csv_header.split(',')
+
+
+def test_header_is_uppercase(csv_header):
     """Column names in header are upeprcase"""
-    header = csv_data[0]
-    assert header == header.upper()
+    assert csv_header == csv_header.upper()
 
-def test_header_starts_with_id(csv_data):
+def test_header_starts_with_id(column_names):
     """First column in header is ID"""
-    header = csv_data[0]
-    column_names = header.split(',')
     first_column_name = column_names[0]
     assert first_column_name == 'ID'
 
-def test_header_has_column_created(csv_data):
+def test_header_has_column_created(column_names):
     """Header has column CREATED"""
-    header = csv_data[0]
-    column_names = header.split(',')
     assert 'CREATED' in column_names
 
-def test_header_has_column_updated(csv_data):
+def test_header_has_column_updated(column_names):
     """Header has column UPDATED"""
-    header = csv_data[0]
-    column_names = header.split(',')
     assert 'UPDATED' in column_names
 
-def test_record_matches_header(csv_data):
+def test_record_matches_header(column_names, csv_records):
     """Number of columns in each record matches header"""
-    header = csv_data[0]
-    column_names = header.split(',')
     header_column_count = len(column_names)
     errors = []
-    for record in csv_data[1:]:
+    for record in csv_records:
         record_values = record.split(',')
         record_values_count = len(record_values)
         if record_values_count != header_column_count:
             errors.append(record)
     assert not errors
 
-def test_record_first_field_is_number(csv_data):
+def test_record_first_field_is_number(csv_records):
     """First value in each record is number"""
     errors = []
-    for record in csv_data[1:]:
+    for record in csv_records:
         record_values = record.split(',')
         if not record_values[0].isdigit():
             errors.append(record)
