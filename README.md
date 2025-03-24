a44095b3bbcfab9e438eefeacdbd3ad8f1f3f6b6
ENIGMA\fszymanski <filip.szymanski@enigma.com.pl>
2025-03-24 14:19:23 +0100

    first test of CSV file - all assertion true

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Python_0
 This repository contains my experiences with Python
 
-First try of editing
+## Commands used: 
+
+### Virtual environment
+- python -m venv venv       #make v.environment
+- venv\Scripts\activate.bat     #activatee v.environment
+- pip install -r pytest-parametrize-example/requirements.txt        #requirements installation
+- python -m pytest      #running tests
---
+++ my_test.py
@@ -0,0 +1,28 @@
+import pytest
+
+# Data verification in CSV file
+
+
+def test_header_is_uppercase():
+    """Column names in header are upeprcase"""
+    assert True
+
+def test_header_starts_with_id():
+    """First column in header is ID"""
+    assert True
+
+def test_header_has_column_created():
+    """Header has column CREATED"""
+    assert True
+
+def test_header_has_column_updated():
+    """Header has column UPDATED"""
+    assert True
+
+def test_record_matches_header():
+    """Number of columns in each record matches header"""
+    assert True
+
+def test_record_first_field_is_number():
+    """First value in each record is number"""
+    assert True
